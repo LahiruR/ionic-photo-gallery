ddd7f447c2bc63dcfcecdb089684dfffc1417fa4
lahiruramesh <lahiru@insureme.lk>
2021-08-08 22:14:46 +0530

    init commit

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,41 @@
 import './ExploreContainer.css';
 
+import { camera, trash, close, images } from 'ionicons/icons';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+         IonFab, IonFabButton, IonIcon, IonGrid, IonRow,
+         IonCol, IonImg, IonText } from '@ionic/react';
+
+import { usePhotoGallery } from "../hooks/useGalleryPhotos";
+
+
 interface ContainerProps {
   name: string;
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  const { takePhoto, photos } = usePhotoGallery();
+
+   console.log('photos', photos);
+  
   return (
     <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    
+      <IonContent>
+          <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={ () => takePhoto() } >
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+          <IonGrid> 
+              <IonRow>
+                {photos.map((photo, index) => (
+                  <IonCol size="6" key={index}>
+                    <IonImg src={photo.webviewPath} />
+                  </IonCol>
+                ))}
+              </IonRow>
+            </IonGrid>
+          </IonFab>
+        </IonContent>
     </div>
   );
 };
---
+++ src/hooks/useGalleryPhotos.ts
@@ -0,0 +1,125 @@
+
+import {
+  Camera,
+  CameraPhoto,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from "@capacitor/camera";
+import { useState, useEffect } from 'react';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+import { Capacitor } from '@capacitor/core';
+import { isPlatform } from "@ionic/react";
+
+
+export interface UserPhoto {
+    filepath: string;
+    webviewPath?: string;
+  }
+
+
+const PHOTO_STORAGE = "photos";
+
+export function usePhotoGallery() {
+    
+    const [photos, setPhotos] = useState<UserPhoto[]>([]);
+
+    useEffect(() => {
+        const loadSaved = async () => {
+          const { value } = await Storage.get({ key: PHOTO_STORAGE });
+          console.log('log', value);
+          
+          if(value) {
+            const photosInStorage = (value ? JSON.parse(value) : []) as UserPhoto[];
+            console.log('log1', photosInStorage);   
+            if (!isPlatform('hybrid')) {
+              for (let photo of photosInStorage) {
+                const file = await Filesystem.readFile({
+                  path: photo.filepath,
+                  directory: Directory.Data
+                });
+                // Web platform only: Load the photo as base64 data
+                photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+              }
+            }
+            setPhotos(photosInStorage);
+          }
+          
+        };
+        loadSaved();
+      }, []);
+
+    const takePhoto = async () => {
+       const cameraPhoto = await Camera.getPhoto({
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera,
+        quality: 100,
+      });
+
+      const fileName = new Date().getTime() + ".jpeg";
+      const savedFileImage = await savePicture(cameraPhoto, fileName); 
+      const newPhotos = [
+        savedFileImage,
+        ...photos,
+        ];
+        Storage.set({ key: PHOTO_STORAGE, value: JSON.stringify(newPhotos) }); 
+        setPhotos(newPhotos); 
+    };
+    
+    return {
+        photos,
+        takePhoto,
+      };
+  }
+
+  const savePicture = async (photo: CameraPhoto, fileName: string): Promise<UserPhoto> => {
+    let base64Data: string;
+    if (isPlatform('hybrid')) {
+        const file = await Filesystem.readFile({
+          path: photo.path!
+        });
+        base64Data = file.data;
+      } else {
+        base64Data = await base64FromPath(photo.webPath!);
+      }
+    
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+    if (isPlatform('hybrid')) {
+        // Display the new image by rewriting the 'file://' path to HTTP
+        // Details: https://ionicframework.com/docs/building/webview#file-protocol
+        return {
+          filepath: savedFile.uri,
+          webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+        };
+      }
+      else {
+        // Use webPath to display the new image instead of base64 since it's
+        // already loaded into memory
+        return {
+          filepath: fileName,
+          webviewPath: photo.webPath
+        };
+      }
+  }; 
+
+  export async function base64FromPath(path: string): Promise<string> {
+    const response = await fetch(path);
+    const blob = await response.blob();
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          resolve(reader.result);
+        } else {
+          reject('method did not return a string')
+        }
+      };
+      reader.readAsDataURL(blob);
+    });
+}
